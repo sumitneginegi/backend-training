15167c579a2cdd27381727390fb9fcd8e5f1b058
sumit <nsumit181993@gmail.com>
2022-08-25 08:38:05 +0530

    a

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+//var address = require('address');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,15 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
+
+        console.log('Before log-timestamp')
+        require('log-timestamp')
+        //console.log('After log-timestamp')
+
+        console.log("ip",req.ip,"URL: ", req.originalUrl);
+      
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
