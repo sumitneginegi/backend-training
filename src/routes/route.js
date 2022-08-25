94c07bc72d51ec2d4ee7d5f14056c4dc0ffba736
sumit <nsumit181993@gmail.com>
2022-08-25 13:10:31 +0530

    a

--- src/routes/route.js
+++ src/routes/route.js
@@ -21,22 +21,22 @@ router.post("/createBook", BookController.createBook  )
 // router.get("/getUsersData", UserController.getUsersData)
 
 
-// const mid1= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid1")
-//     // logic
-//     let loggedIn = false
+const mid1= function ( req, res, next) {
+    console.log("Hi I am a middleware named Mid1")
+    // logic
+    let loggedIn = false
 
-//     if (loggedIn== true) { 
-//         console.log( "OK LOGGED IS IS TRUE NOW")
-//         next ()
-//     }
-//     else {
-//         res.send ("Please login or register")
-//     }
-// }
+    if (loggedIn== true) { 
+        console.log( "OK LOGGED IS IS TRUE NOW")
+        next ()
+    }
+    else {
+        res.send ("Please login or register")
+    }
+}
 
 // // e.g. restricted and open-to-all API's can be handled like below now:
-// router.get('/homePage', mid1, UserController.feeds)
+router.get('/homePage', mid1, UserController.feeds)
 // router.get('/profileDetails', mid1, UserController.profileDetails)
 // router.get('/friendList', mid1, UserController.friendList)
 // router.get('/changePassword', mid1, UserController.changePassword)
@@ -48,7 +48,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
